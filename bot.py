a4645d0746e1be5d17d7f001c538832ae11b91a1
lama_imp <lakeyeva.marina@gmail.com>
2021-05-31 17:54:49 +0400

    bot.py edited

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import telebot
 from telebot import types
 from SQLighter import SQLighter
-from config import token
-from verb_search import possible_answer, database_name
+from config import token, database_name
+from verb_search import possible_answer
 
 
 bot = telebot.TeleBot(token)
